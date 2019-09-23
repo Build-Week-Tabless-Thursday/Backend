68f399e913ace05e8da289b83b41323d55019a0f
dvcolin <46186956+dvcolin@users.noreply.github.com>
2019-09-23 13:52:12 -0700

    minor changes in db-config

--- data/db-config.js
+++ data/db-config.js
@@ -1,7 +1,10 @@
 const knex = require('knex');
+const secrets = require('../secrets/secrets.js');
+
+const environment = secrets.environment || 'development';
 
 const knexFile = require('../knexfile.js');
 
-const knexConfig = knexFile.development;
+const config = knexFile[environment];
 
-module.exports = knex(knexConfig);
+module.exports = knex(config);
--- secrets/secrets.js
+++ secrets/secrets.js
@@ -2,5 +2,6 @@ require('dotenv').config();
 
 
 module.exports = {
-    JWT_SECRET: process.env.JWT_SECRET
+    JWT_SECRET: process.env.JWT_SECRET,
+    environment: process.env.NODE_ENV
 }
