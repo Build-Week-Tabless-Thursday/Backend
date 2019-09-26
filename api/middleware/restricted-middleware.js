f981308cf861bbb9d1f5c9bdcdff5afc3a97702f
dvcolin <46186956+dvcolin@users.noreply.github.com>
2019-09-25 23:50:20 -0700

    added code from williams branch. added screenshot token to env file

--- api/middleware/restricted-middleware.js
+++ api/middleware/restricted-middleware.js
@@ -1,17 +1,17 @@
-const jwt = require("jsonwebtoken");
-const secrets = require("../../secrets/secrets.js");
+const jwt = require('jsonwebtoken');
+const secrets = require('../../secrets/secrets.js');
 
-const Users = require("../../models/users-model.js");
+const Users = require('../../models/users-model.js');
 
 module.exports = (req, res, next) => {
   const token = req.headers.authorization;
 
   if (token) {
     jwt.verify(token, secrets.JWT_SECRET, (err, decodedToken) => {
-      if (err) res.status(401).json({ message: "Invalid Token" });
+      if (err) res.status(401).json({ message: 'Invalid Token' });
       Users.findById(decodedToken.id).then(user => {
         if (!user) {
-          res.status(401).json({ message: "Invalid Credentials" });
+          res.status(401).json({ message: 'Invalid Credentials' });
         } else {
           console.log(user);
           req.user = {
@@ -24,6 +24,6 @@ module.exports = (req, res, next) => {
       });
     });
   } else {
-    res.status(401).json({ message: "Invalid Credentials" });
+    res.status(401).json({ message: 'Invalid Credentials' });
   }
 };
--- api/routes/test/user-router.test.js
+++ api/routes/test/user-router.test.js
@@ -1,19 +1,19 @@
-const request = require("supertest");
-const server = require("../../server.js");
+const request = require('supertest');
+const server = require('../../server.js');
 
-describe("user-router.js", () => {
-  describe("GET /me", () => {
-    it("should return a JSON object", () => {
+const Users = require('../../../models/users-model.js');
+
+describe('user-router.js', () => {
+  describe('GET /me', () => {
+    it('should return a JSON object', () => {
       let response;
       return request(server)
-        .get("/me")
+        .get('/me')
         .then(res => {
           response = res;
 
-          expect(response.type).toEqual("application/json");
+          expect(response.type).toEqual('application/json');
         });
     });
-
-    it("should return a 200 status code if successful", () => {});
   });
 });
