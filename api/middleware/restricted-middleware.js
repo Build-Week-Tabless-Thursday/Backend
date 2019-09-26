efcbb9f366786ad3fc5e75c5131c968ef9f7bc03
William Sedlacek <wsedlacekc@gmail.com>
2019-09-26 10:04:13 -0500

    Better error handling on for auth

--- api/middleware/restricted-middleware.js
+++ api/middleware/restricted-middleware.js
@@ -1,29 +1,33 @@
-const jwt = require("jsonwebtoken");
-const secrets = require("../../secrets/secrets.js");
+const jwt = require('jsonwebtoken');
+const secrets = require('../../secrets/secrets.js');
 
-const Users = require("../../models/users-model.js");
+const Users = require('../../models/users-model.js');
 
 module.exports = (req, res, next) => {
-  const token = req.headers.authorization;
+  try {
+    const token = req.headers.authorization;
+    if (token) {
+      jwt.verify(token, secrets.JWT_SECRET, (err, decodedToken) => {
+        if (err || !decodedToken.id) throw new Error('Invalid Token');
 
-  if (token) {
-    jwt.verify(token, secrets.JWT_SECRET, (err, decodedToken) => {
-      if (err) res.status(401).json({ message: "Invalid Token" });
-      Users.findById(decodedToken.id).then(user => {
-        if (!user) {
-          res.status(401).json({ message: "Invalid Credentials" });
-        } else {
-          console.log(user);
-          req.user = {
-            id: decodedToken.id,
-            username: decodedToken.username,
-            email: decodedToken.email
-          };
-          next();
-        }
+        Users.findById(decodedToken.id).then(user => {
+          if (!user) {
+            throw new Error('Invalid Token');
+          } else {
+            console.log(user);
+            req.user = {
+              id: decodedToken.id,
+              username: decodedToken.username,
+              email: decodedToken.email,
+            };
+            next();
+          }
+        });
       });
-    });
-  } else {
-    res.status(401).json({ message: "Invalid Credentials" });
+    } else {
+      throw new Error('Invalid Credentials');
+    }
+  } catch (err) {
+    res.status(401).json({ message: err.toString() });
   }
 };
